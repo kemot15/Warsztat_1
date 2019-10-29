ba0a77e489ff6be166dced89c2c7f6996b15eb32
Tomek <kemot15@tlen.pl>
2019-10-29 14:12:42 +0100

    Add project files.
    Add .gitignore and .gitattributes.

---
+++ Tasks/Flag.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasks
+{
+    enum Flag
+    {
+        IMPORTANT, NORAML, NOTIMPORTANT
+    }
+}
---
+++ Tasks/TaskModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasks
+{
+    class TaskModel
+    {
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsAllDayFlag { get; set; }
+        public Flag ImportantFlag { get; set; }
+    }
+}
